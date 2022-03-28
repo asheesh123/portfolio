2778aa42bab69d8f0800f8a1853cf37be5bfae41
Asheesh <asheeshvishvkarma123@gmail.com>
2022-03-28 21:17:24 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <img src={"./bg-image.jpg"} className="bg-image" alt="logo" />
+    </>
+  );
+}
+
+export default App;
